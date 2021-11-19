f07515d1f7dd8bbc62dabd3e7e02fcc0addea736
zoyafuso-NOAA <60302777+zoyafuso-NOAA@users.noreply.github.com>
2021-11-18 23:40:01 -0800

    updated

--- presentations/result_11_23_2021/SS_tradeoffs.R
+++ presentations/result_11_23_2021/SS_tradeoffs.R
@@ -1,12 +1,12 @@
-load("data/survey_opt_data/optimization_data.RData")
+load("data/survey_opt_data/optimization_data_otter.RData")
 
 par(mfrow = c(3, 2), mar = c(3, 3, 1, 1))
 for (ispp in spp_list) {
-  n_runs <- length(dir(paste0("results/otter_trawl/survey_opt/SS/", ispp, "/")))
+  # n_runs <- length(dir(paste0("results/otter_trawl/survey_opt/SS/", ispp, "/")))
   
-  cv_n <- data.frame(run = 1:n_runs)
+  cv_n <- data.frame(run = 1:4)
   
-  for (irun in 1:n_runs){
+  for (irun in 1:4){
     load(paste0("results/otter_trawl/survey_opt/SS/", ispp, 
                 "/Run_", irun, "/result_list.RData"))
     cv_n$cv[irun] <- result_list$cvs
