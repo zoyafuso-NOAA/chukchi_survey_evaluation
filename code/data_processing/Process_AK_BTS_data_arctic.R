8d49384be644f43b716babfdbc29d83cdd4bda2e
zoyafuso-NOAA <60302777+zoyafuso-NOAA@users.noreply.github.com>
2022-03-10 10:09:05 -0800

    changed effort calc, spp. specific data

--- code/data_processing/Process_AK_BTS_data_arctic.R
+++ code/data_processing/Process_AK_BTS_data_arctic.R
@@ -3,19 +3,7 @@
 ## Author:        Zack Oyafuso (zack.oyafuso@noaa.gov), modfied from 
 ##                Lewis Barnett (lewis.barnett@noaa.gov)
 ##
-## Notes:         2012 is only year where both beam and 83-112 otter trawls 
-##                were conducted in the Chukchi. 83-112 trawl data includes 
-##                many values of GEAR: c(30:44,47,178,180,181). Code 30 from 
-##                1975-1982/1985 then small changes through to code 44 used for
-##                current EBS survey; code 47 not present in database. Code 178
-##                and 180 from 1998 only; code 181 not present in database. 
-##                Beam trawl is GEAR_CODE 180 (when fished as extension of 
-##                otter trawl, unclear whether code is for otter trawl only or
-##                not), all from year 1998 in BS REGION (also 179 in codebook 
-##                but not present in data). Plum staff beam trawl primarily 218 
-##                and 219 (Norcross; but only have year 2012, when also used 
-##                gear code 44), 217 also similar (but only have handful in 
-##                1989 for BS region)
+## Notes:         
 ###############################################################################
 rm(list = ls())
 
@@ -33,6 +21,12 @@ haul <- read.csv(file = "data/fish_data/AK_BTS_OtterAndBeam/haul.csv",
                  stringsAsFactors = FALSE)
 catch <- read.csv(file = "data/fish_data/AK_BTS_OtterAndBeam/catch_subsetted_BS.csv", 
                   stringsAsFactors = FALSE)
+
+###############################################################################
+####   Create output result directory if does not exist already
+############################################################################### 
+output_dir <- "data/fish_data/AK_BTS_OtterAndBeam/data_long_by_taxa/"
+if (!dir.exists(output_dir)) dir.create(output_dir)
 
 ##################################################
 ####   Some manipulations of the haul dataframe:
@@ -60,6 +54,8 @@ haul <- subset(x = haul,
 species_codes <- 
   read.csv(file = "data/fish_data/AK_BTS_OtterAndBeam/species.csv", 
            stringsAsFactors = FALSE)
+taxa <- read.csv(file = "data/fish_data/AK_BTS_OtterAndBeam/taxonomy.csv", 
+                 stringsAsFactors = FALSE)
 species_codes <- dplyr::select(.data = species_codes, -YEAR_ADDED)
 catch <- dplyr::left_join(x = catch, y = species_codes)
 dat <- dplyr::left_join(x = haul, y = cruise, by = 'CRUISEJOIN')
@@ -86,18 +82,12 @@ dat$GEAR_CAT <- as.factor(dat$GEAR_CAT)
 ####   Join haul and catch data for selected species in species_list, 
 ####   summarize CPUE in numbers or kg per square km currently 
 ##################################################
-species_list <- c("Arctic cod", "saffron cod", "Pacific cod", "walleye pollock",
-                  "snow crab", "yellowfin sole","Alaska plaice", 
-                  "Bering flounder")
-
 dat <- dplyr::left_join(x = dat, y = catch) %>%
-  dplyr::mutate(CPUE_KG = WEIGHT / (DISTANCE_FISHED * NET_WIDTH * 0.001), 
-                CPUE_N = NUMBER_FISH / (DISTANCE_FISHED * NET_WIDTH * 0.001)) %>%
-  dplyr::filter(COMMON_NAME %in% species_list,
-                !is.na(CPUE_KG),
-                !is.na(CPUE_N))
+  dplyr::mutate(CPUE_KG = WEIGHT / (DISTANCE_FISHED * NET_WIDTH * 0.001),
+                CPUE_N = NUMBER_FISH / (DISTANCE_FISHED * NET_WIDTH * 0.001))
 
 dat$AREA_SWEPT <- dat$DISTANCE_FISHED * dat$NET_WIDTH * 0.001
+dat <- dat[!is.na(dat$AREA_SWEPT), ]
 
 ##################################################
 #### Calculate station location as the mean of the start and ending points
@@ -109,67 +99,136 @@ dat$MEAN_LONGITUDE <- rowMeans(dat[, c("START_LONGITUDE", "END_LONGITUDE")])
 #### Subset gear and year-specific data
 ##################################################
 dat <- subset(dat,
-              select = c(SURVEY_NAME, YEAR, GEAR_CAT,
+              select = c(SURVEY_NAME, YEAR, GEAR_CAT, HAULJOIN,
                          STATIONID, MEAN_LONGITUDE, MEAN_LATITUDE,
                          DATE, MONTH, DAY, YEAR,
                          GEAR_DEPTH, GEAR_TEMPERATURE,
-                         COMMON_NAME, SPECIES_NAME, CPUE_KG, CPUE_N, 
-                         AREA_SWEPT))
+                         SPECIES_CODE, 
+                         CPUE_KG, CPUE_N, AREA_SWEPT))
 
 ##################################################
 #### Create a wide df with zeros filled for stations where species not observed
 ##################################################
-dat$STATION_ID <- with(dat, paste(MEAN_LONGITUDE, MEAN_LATITUDE))
+# dat$STATION_ID <- with(dat, paste(MEAN_LONGITUDE, MEAN_LATITUDE))
 
 cpue_wide <- tidyr::spread(
   data = dat[, c("YEAR", "MONTH", "DAY", "DATE",
-                 "STATION_ID", "CPUE_KG", "AREA_SWEPT",
-                 "COMMON_NAME", "GEAR_CAT")], 
-  key = COMMON_NAME, 
+                 "HAULJOIN", "CPUE_KG", "AREA_SWEPT",
+                 "SPECIES_CODE", "GEAR_CAT")], 
+  key = SPECIES_CODE, 
   value = "CPUE_KG", 
   fill = 0)
 
 cpue_wide <- cpue_wide[order(cpue_wide$GEAR_CAT,
-                             cpue_wide$STATION_ID), ]
+                             cpue_wide$HAULJOIN), ]
 
 ##################################################
 ## attach station specific data to data_wide
 ##################################################
-station_data <- dat[!duplicated(dat$STATION_ID), 
-                    c("STATION_ID", "SURVEY_NAME", 
+station_data <- dat[!duplicated(dat$HAULJOIN) ,
+                    c("HAULJOIN", "SURVEY_NAME", 
                       "DATE", "YEAR", "MONTH", "DAY", 
                       "GEAR_CAT", "GEAR_DEPTH", "GEAR_TEMPERATURE",  
-                      "MEAN_LONGITUDE", "MEAN_LATITUDE", "AREA_SWEPT") ]
+                      "MEAN_LONGITUDE", "MEAN_LATITUDE") ]
 
 station_data <- station_data[order(station_data$GEAR_CAT,
-                                   station_data$STATION_ID), ]
+                                   station_data$HAULJOIN), ]
 
-data_wide <- cbind(station_data, 
-                   subset(cpue_wide, select = -STATION_ID))
+data_wide <- cbind(station_data, cpue_wide)
 
-##################################################
-#### "lengthen" wide dataset to long-form to match VAST data input 
-##################################################
-data_long <- reshape::melt(
-  data = data_wide[, c("YEAR", "MONTH", "DAY", "DATE", "GEAR_CAT", 
-                       "MEAN_LONGITUDE", "MEAN_LATITUDE", 
-                       "GEAR_DEPTH", "GEAR_TEMPERATURE", "AREA_SWEPT",
-                       unique(dat$COMMON_NAME))],
-  measure.vars = unique(dat$COMMON_NAME),
-  variable_name = "COMMON_NAME")
+###############################################################################
+####   Loop over individual species, save data inputs to file and remove from
+####      from the cpue_wide. 
+###############################################################################
+species_info <- data.frame()
 
-names(data_long) <- c("year", "month", "day", "date", "gear", "lon", "lat", 
-                      "bot_depth", "bot_temp", "actual_area_swept",
-                      "common_name", "cpue_kg_km2")
-data_long$area_swept_km2 <- 1
+solo_spp <- subset(x = species_codes, 
+                   subset = COMMON_NAME %in% 
+                     c("Arctic cod",  "Pacific cod", "walleye pollock", 
+                       "snow crab", "yellowfin sole", "Alaska plaice", 
+                       "Bering flounder", "saffron cod",
+                       "Pacific herring",
+                       "purple-orange sea star") )
 
-##################################################
-####   Save
-##################################################
-write.csv(x = data_long, 
-          file = "data/fish_data/AK_BTS_OtterAndBeam/AK_BTS_Arctic_processed_long.csv", 
-          row.names = FALSE)
-write.csv(x = data_wide, 
-          file = "data/fish_data/AK_BTS_OtterAndBeam/AK_BTS_Arctic_processed_wide.csv", 
-          row.names = FALSE)
+for (irow in 1:nrow(solo_spp)) {
+  
+  ispp_code <- solo_spp$SPECIES_CODE[irow]
+  ispp_sci_name <- solo_spp$SPECIES_NAME[irow]
+  ispp <- solo_spp$COMMON_NAME[irow]
+  
+  species_info <- rbind(species_info, 
+                        data.frame(common_name = ispp,
+                                   scientific_name = ispp_sci_name,
+                                   file_name = ispp,
+                                   species_code = ispp_code))
+  
+  if (ispp_code %in% names(data_wide)) {
+    data_long <- data_wide[, c("YEAR", "MONTH", "DAY", "DATE", "GEAR_CAT", 
+                               "MEAN_LONGITUDE", "MEAN_LATITUDE", "HAULJOIN", 
+                               "GEAR_DEPTH", "GEAR_TEMPERATURE", 
+                               "AREA_SWEPT", ispp_code)]
+    names(data_long) <- c("year", "month", "day", "date", "gear", "lon", "lat", 
+                          "hauljoin", "bot_depth", "bot_temp", 
+                          "actual_area_swept", "cpue_kg_km2")
+    
+    ## Remove species column from data_wide
+    data_wide <- data_wide[!names(data_wide) %in% paste(ispp_code)]
+    
+    ## Save data
+    write.csv(x = data_long, file = paste0(output_dir, ispp, ".csv"), 
+              row.names = F) 
+  }
+}
 
+###############################################################################
+####   Aggregate species
+############################################################################### 
+aggregate_species <- read.csv(file = "data/fish_data/aggregates_species.csv")
+for (irow in 1:nrow(aggregate_species)) {
+  
+  species_code_query <- paste0("subset(x = taxa, subset = ", 
+                               aggregate_species$species_query[irow], ")")
+  species_query <- eval(parse(text = species_code_query))
+  species_query <- 
+    species_query[species_query$SPECIES_CODE %in% names(data_wide), ]
+  
+  if (nrow(species_query) > 0) {
+    ispp <- aggregate_species$taxon_name[irow]
+    ispp_code <- species_query$SPECIES_CODE
+    ispp_common <- species_query$REPORT_NAME_SCIENTIFIC 
+    ispp_sci_name <- species_query$REPORT_NAME_SCIENTIFIC
+    
+    species_info <- rbind(species_info, 
+                          data.frame(common_name = ispp,
+                                     scientific_name = ispp_sci_name,
+                                     file_name = ispp,
+                                     species_code = ispp_code))
+    
+    
+    sub_df <- as.matrix(data_wide[, paste(ispp_code)])
+    
+    data_long <- cbind(data_wide[, c("YEAR", "MONTH", "DAY", "DATE", 
+                                     "GEAR_CAT", "MEAN_LONGITUDE", 
+                                     "MEAN_LATITUDE", "HAULJOIN", 
+                                     "GEAR_DEPTH", "GEAR_TEMPERATURE", 
+                                     "AREA_SWEPT")],
+                       cpue_kg_km2 = rowSums(sub_df))
+    names(data_long) <- c("year", "month", "day", "date", "gear", "lon", "lat",
+                          "hauljoin", "bot_depth", "bot_temp", 
+                          "actual_area_swept", "cpue_kg_km2")
+    
+    ## Remove species column from data_wide
+    data_wide <- data_wide[!names(data_wide) %in% paste(ispp_code)]
+    
+    ## Save data
+    write.csv(x = data_long, file = paste0(output_dir, ispp, ".csv"),
+              row.names = F) 
+  }
+}
+
+###############################################################################
+####   Save species info data
+###############################################################################  
+write.csv(x = species_info, 
+          file = paste0(dirname(output_dir), "/species_info.csv"),
+          row.names = F)
