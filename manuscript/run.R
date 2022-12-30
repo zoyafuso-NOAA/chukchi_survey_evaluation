272de31282e60bda2e79580cba76b0d9e1f12b20
zoyafuso-NOAA <60302777+zoyafuso-NOAA@users.noreply.github.com>
2022-12-29 23:04:54 -0800

    removed sea cukes and other sea stars
    changed name of corals

--- manuscript/run.R
+++ manuscript/run.R
@@ -1,8 +1,15 @@
 library(rmarkdown)
+library(tidyr)
+library(kableExtra)
+library(FishStatsUtils)
 
 draft_version <- 0
 draft_dir <- paste0("manuscript/Version_", draft_version, "/")
 if(!dir.exists(draft_dir)) dir.create(draft_dir)
+
+file.copy(from = "figures/",
+          to = draft_dir,
+          recursive = TRUE)
 
 rmarkdown::render(input = "manuscript/Oyafuso_etal_Chukchi_survey_testing.Rmd", 
                   output_format = "word_document", 
@@ -10,7 +17,5 @@ rmarkdown::render(input = "manuscript/Oyafuso_etal_Chukchi_survey_testing.Rmd",
                                       "Oyafuso_etal_Chukchi_survey_testing.docx"),
                   params = list(draft_dir = draft_dir))
 
-file.copy(from = "figures/",
-          to = draft_dir,
-          recursive = TRUE)
+
 
