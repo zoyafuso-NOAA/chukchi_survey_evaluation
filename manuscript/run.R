b022cbdba5ad774d9913f9eb5e0ca483a37aa57a
zoyafuso-NOAA <60302777+zoyafuso-NOAA@users.noreply.github.com>
2023-02-02 09:35:54 -0800

    added comments, appendices A and B

--- manuscript/run.R
+++ manuscript/run.R
@@ -1,21 +1,54 @@
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+## Project:       
+## Author:        Zack Oyafuso (zack.oyafuso@noaa.gov)
+## Description:
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+## Restart R Session before running
+rm(list = ls())
+
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##   Import Libraries
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 library(rmarkdown)
 library(tidyr)
 library(kableExtra)
 library(FishStatsUtils)
 
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##   Set draft version
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 draft_version <- 0
 draft_dir <- paste0("manuscript/Version_", draft_version, "/")
 if(!dir.exists(draft_dir)) dir.create(draft_dir)
 
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##   Copy the most current version of the figures into the draft version folder
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 file.copy(from = "figures/",
           to = draft_dir,
           recursive = TRUE)
 
-rmarkdown::render(input = "manuscript/Oyafuso_etal_Chukchi_survey_testing.Rmd", 
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##   Render the main manuscript text
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+rmarkdown::render(input = "manuscript/Oyafuso_etal_Chukchi_MS.Rmd", 
                   output_format = "word_document", 
                   output_file = paste0("../", draft_dir,
-                                      "Oyafuso_etal_Chukchi_survey_testing.docx"),
+                                       "Oyafuso_etal_Chukchi_MS.docx"),
                   params = list(draft_dir = draft_dir))
 
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##   Render Appendix A and B
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+rmarkdown::render(input = "manuscript/Oyafuso_etal_Chukchi_Appendix_A.Rmd", 
+                  output_format = "word_document", 
+                  output_dir =  draft_dir, 
+                  output_file = "Appendix_A.docx")
 
+rmarkdown::render(input = "manuscript/Oyafuso_etal_Chukchi_Appendix_B.Rmd", 
+                  output_format = "word_document", 
+                  output_dir = draft_dir,
+                  output_file = "Appendix_B.docx",
+                  params = list(draft_dir = draft_dir))
