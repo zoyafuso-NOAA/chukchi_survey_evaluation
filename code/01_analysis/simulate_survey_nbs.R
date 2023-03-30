3d8ce1ce6a88fd777d0830c375a3271641966449
Lewis Barnett <labarnett@ucdavis.edu>
2023-03-30 11:47:48 -0700

    init nbs

--- code/01_analysis/simulate_survey_nbs.R
+++ code/01_analysis/simulate_survey_nbs.R
@@ -25,19 +25,18 @@ library(terra)
 ## Load input data ----
 ## Define areas of cells, total area
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-load("data/survey_opt_data/optimization_data_nbs.RData")
+load(here::here("data/survey_opt_data/optimization_data_nbs.RData"))
 cell_area <- FishStatsUtils::northern_bering_sea_grid[, "Area_in_survey_km2"]
 total_area <- sum(cell_area)
 
-grid_pts <- terra::vect("data/survey_opt_data/grid_pts_nbs.shp")
+grid_pts <- readOGR(here::here("data/survey_opt_data/grid_pts_nbs.shp"))
 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-##   Create an outline of the Chukchi grid in aea projection. This mask is used
+##   Create an outline of the NBS grid in aea projection. This mask is used
 ##       when creating the systematic grids. 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 nbs_mask <-
-  rgdal::readOGR("data/spatial_data/survey_boundary/NBS_2019.shp")
-crs(nbs_mask) <- aea_crs
+  rgdal::readOGR(here::here("data/spatial_data/survey_boundary/NBS_2019.shp"))
 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ## Set constants ----
@@ -48,6 +47,7 @@ crs(nbs_mask) <- aea_crs
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  
 
 igear = c("otter", "beam")[1]
+iyear <- "2022"
 
 true_index <- true_index[nrow(true_index), ]
 
@@ -56,12 +56,12 @@ true_index <- true_index[nrow(true_index), ]
 ## Collate simulated densities across species for a given igear.
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  
 ms_dens <- array(dim = c(n_spp, n_cells, n_iters), 
-                 dimnames = list(spp_list, NULL))
+                 dimnames = list(spp_list$taxon, NULL))
 
 for (ispp in 1:n_spp) { ## Loop over species -- start
-  load(paste0("results/nbs_", igear,
+  load(here::here(paste0("results/nbs_", igear,
               "/vast_fits/", spp_list$taxon[ispp], "/simulated_densities/sim_data_",
-              iyear, "_simtype1.RData"))
+              iyear, "_simtype1.RData")))
   
   ms_dens[ispp, , ] <- get(paste0("sim_data_", iyear, "_simtype1"))
 } ## Loop over species -- end
@@ -73,7 +73,7 @@ for (ispp in 1:n_spp) { ## Loop over species -- start
 target_n <- seq(from = 55, to = 175, by = 15)
 index_srs <- cv_srs <- rb_index_srs <-
   array(dim = c(length(target_n), n_spp, n_iters),
-        dimnames = list(paste0("srs_n = ", target_n), spp_list, NULL))
+        dimnames = list(paste0("srs_n = ", target_n), spp_list$taxon, NULL))
 
 set.seed(234)
 for (isample in 1:length(target_n)) { ## Loop over total effort -- start
@@ -114,13 +114,6 @@ temp_res <- 65000
 index_sys <- cv_sys <- cv_sys_LO5 <- rb_index_sys <- list()
 sys_settings <- NULL
 
-##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-##   Create points within NBS mask
-##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  
-grid_pts_aea <- sp::SpatialPointsDataFrame(coords = grid_pts@coords,
-                                           data = data.frame(id = 1:n_cells),
-                                           proj4string = aea_crs)
-
 
 set.seed(54904)
 while (temp_res >= 35000) {
@@ -128,15 +121,14 @@ while (temp_res >= 35000) {
   ## Temporary result objects for a given grid resolution
   temp_index_sys <- temp_cv_sys <- temp_cv_sys_LO5 <- temp_rb_index_sys <-
     array(dim = c(n_spp, n_iters),
-          dimnames = list(spp_list, NULL))
+          dimnames = list(spp_list$taxon, NULL))
   
   ## Make station grid based on latlon coords, random start
-  temp_grid <- sp::makegrid(x = grid_pts_aea, 
+  temp_grid <- sp::makegrid(x = grid_pts, 
                             cellsize = temp_res, 
                             pretty = F)
-  temp_grid <- sp::SpatialPoints(coords = coordinates(temp_grid),
-                                 proj4string = aea_crs)
-  temp_grid <- terra::intersect(x = temp_grid, y = chukchi_mask)
+  temp_grid <- sp::SpatialPoints(coords = coordinates(temp_grid))
+  temp_grid <- terra::intersect(x = temp_grid, y = nbs_mask)
   
   plot(temp_grid)
   
@@ -147,14 +139,14 @@ while (temp_res >= 35000) {
                                    res = temp_res))
   
   ## Calculate the grid cell that falls under the each station
-  grid_idx <- apply(X = rgeos::gDistance(spgeom1 = grid_pts_aea,
+  grid_idx <- apply(X = rgeos::gDistance(spgeom1 = grid_pts,
                                          spgeom2 = temp_grid,
                                          byid = TRUE),
                     MARGIN = 1,
                     FUN = which.min)
   
   ## Calculate the nearest neighbor stations for each station
-  distances <- as.matrix(dist(grid_pts_aea[grid_idx, ]@coords))
+  distances <- as.matrix(dist(grid_pts[grid_idx, ]@coords))
   min_distances <- apply(X = distances,
                          MARGIN = 1,
                          FUN = function(x) {
@@ -163,7 +155,7 @@ while (temp_res >= 35000) {
                          })
   
   neighbor_var <- matrix(nrow = length(min_distances), ncol = n_spp,
-                         dimnames = list(NULL, spp_list))
+                         dimnames = list(NULL, spp_list$taxon))
   
   for (iter in 1:n_iters) {
     
@@ -219,7 +211,7 @@ cv_sys_LO5 <- abind::abind(cv_sys_LO5, along = 0)
 rm(sys_sample, sys_mean, sys_sd_LO5, sys_index, sys_sd, 
    temp_grid, distances, 
    temp_res, temp_rb_index_sys, temp_n,
-   temp_index_sys, temp_cv_sys, temp_cv_LO5, temp_cv_sys_LO5)
+   temp_index_sys, temp_cv_sys, temp_cv_sys_LO5)
 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ##   Simumulate STRS ----
@@ -227,13 +219,14 @@ rm(sys_sample, sys_mean, sys_sd_LO5, sys_index, sys_sd,
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  
 index_ms_strs <- cv_ms_strs <- rb_index_ms_strs <-
   array(dim = c(length(target_n), n_spp, n_iters),
-        dimnames = list(paste0("ms_strs_n = ", target_n), spp_list, NULL))
+        dimnames = list(paste0("ms_strs_n = ", target_n), spp_list$taxon, NULL))
 n_strata <- c("otter" = 3, "beam" = 4)[igear]
+n_strata_realized <- 2 # optimization only chose 2 strata
 
-load(paste0("results/chukchi_", igear,
-            "/survey_opt/Str_", n_strata, "/result_list.RData"))
-load(paste0("results/chukchi_", igear,
-            "/survey_opt/Str_", n_strata, "/allocations.RData"))
+load(here::here(paste0("results/nbs_", igear,
+            "/survey_opt/Str_", n_strata, "/result_list.RData")))
+load(here::here(paste0("results/nbs_", igear,
+            "/survey_opt/Str_", n_strata, "/allocations_nbs.RData")))
 
 set.seed(5231)
 for (itarget in target_n){ ## Loop over sampling effort -- start
@@ -241,7 +234,7 @@ for (itarget in target_n){ ## Loop over sampling effort -- start
   ## STRS settings
   solution <- result_list$sol_by_cell
   allocation <- as.integer(subset(ms_sample_allocations, 
-                                  n == itarget)[, paste0("Str_", 1:n_strata)])
+                                  n == itarget)[, paste0("Str_", 1:n_strata_realized)])
   Ah <- tapply(X = cell_area, INDEX = solution, FUN = sum)
   n_strs <- sum(allocation)
   
@@ -414,7 +407,7 @@ for (idesign in c("srs", "ms_strs", "sys")) { ## Loop over design type -- start
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ##   Save results ----
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-save(file = paste0("results/chukchi_", igear, 
+save(file = here::here(paste0("results/nbs_", igear, 
                    "/random_survey_sim_results.RData"),
      list = c(as.vector(sapply(X = c("true_cv_", "rrmse_cv_", 
                                      "index_", "cv_", 
@@ -422,4 +415,4 @@ save(file = paste0("results/chukchi_", igear,
                                FUN = function(x) 
                                  paste0(x, c("srs", "ms_strs", "sys")))),
               "target_n", "sys_settings", 
-              "cv_sys_LO5", "rb_cv_sys_LO5", "rrmse_cv_sys_LO5"))
+              "cv_sys_LO5", "rb_cv_sys_LO5", "rrmse_cv_sys_LO5")))
