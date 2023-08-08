4add3141ecd8f1f7a76a29fbedc4a66fdf3f5639
Lewis Barnett <lewis.barnett@noaa.gov>
2023-08-08 11:48:35 -0700

    combined, standardized data

--- code/00_data_processing/Combine_AK_BTS_data.R
+++ code/00_data_processing/Combine_AK_BTS_data.R
@@ -16,4 +16,11 @@ list_of_files <- c(list.files(path = "data/fish_data/AK_BTS_OtterAndBeam/data_lo
                               full.names = TRUE))
 
 df <- list_of_files %>% 
-  map_dfr(read.csv, header=TRUE, fill=TRUE)
+  map_dfr(read.csv, header=TRUE, fill=TRUE) %>%
+  mutate(catch_kg = area_swept_km2 * cpue_kg_km2)
+
+write.csv(x = df, 
+          file = "data/fish_data/ak_bts_ierp_chukchi_nbs_combined.csv",
+          row.names = F)
+write_rds(x = df, 
+          file = "data/fish_data/ak_bts_ierp_chukchi_nbs_combined.rds")
