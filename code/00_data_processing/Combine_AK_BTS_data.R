6d256726e71a847c0a9fce1e7c261b2a7ff2c6d0
Lewis Barnett <lewis.barnett@noaa.gov>
2023-08-08 09:46:07 -0700

    init

--- code/00_data_processing/Combine_AK_BTS_data.R
+++ code/00_data_processing/Combine_AK_BTS_data.R
@@ -3,3 +3,14 @@
 ## Author:        Lewis Barnett (lewis.barnett@noaa.gov)
 ###############################################################################
 
+library(tidyverse)
+
+list_of_files <- c(list.files(path = "data/fish_data/AK_BTS_OtterAndBeam/data_long_by_taxa/",
+                            pattern = "\\.csv$",
+                            full.names = TRUE),
+                   list.files(path = "data/fish_data/AK_BTS_OtterAndBeam/data_long_by_taxa_nbs/",
+                              pattern = "\\.csv$",
+                              full.names = TRUE))
+
+df <- list_of_files %>% 
+  map_dfr(read.csv, header=TRUE, fill=TRUE)
